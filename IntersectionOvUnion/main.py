b0170c2a01d42e4b4492e1565f65e0ac490ee585
Maxim Bobrin <derivas121@gmail.com>
2020-12-21 16:22:20 +0300

    intersection

---
+++ IntersectionOvUnion/main.py
@@ -0,0 +1,39 @@
+import torch
+
+def intersection_ov_union(boxes_preds, boxes_labels, box_format = "midpoint"):
+    '''
+    :param boxes_preds: shape (N, 4) N - # of boxes
+    :param boxes_labels:
+    :param box_format:
+    :return: in given format
+    '''
+
+    if box_format == "midpoint":
+        box1_x1 = (boxes_preds[:, 0:1] - boxes_preds[:, 2:3]) / 2
+        box1_y1 = (boxes_preds[:,1:2] - boxes_preds[:, 3:4]) / 2
+        box1_x2 = (boxes_preds[:, 0:1] + boxes_preds[:, 2:3]) / 2
+        box1_y2 = (boxes_preds[:, 1:2] + boxes_preds[:, 3:4]) / 2
+        box2_x1 = (boxes_labels[:, 0:1] - boxes_labels[:, 2:3]) / 2
+        box2_y1 = (boxes_labels[:, 1:2] - boxes_labels[:, 3:4]) / 2
+        box2_x2 = (boxes_labels[:, 0:1] + boxes_labels[:, 2:3]) / 2
+        box2_y2 = (boxes_labels[:, 1:2] + boxes_labels[:, 3:4]) / 2
+
+    if box_format == "corners":
+        box1_x1 = boxes_preds[:, 0:1]
+        box1_y1 = boxes_preds[:, 1:2]
+        box1_x2 = boxes_preds[:, 2:3]
+        box1_y2 = boxes_preds[:, 3:4]
+        box2_x1 = boxes_labels[:, 0:1]
+        box2_y1 = boxes_labels[:, 1:2]
+        box2_x2 = boxes_labels[:, 2:3]
+        box2_y2 = boxes_labels[:, 3:4]
+
+    x1 = torch.max(box1_x2, box2_x2)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    inter = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+    box1area = abs((box1_x2 - box1_x1)*(box1_y1 - box1_y2))
+    box2area = abs((box2_x2 - box2_x1) * (box2_y1 - box2_y2))
+    return inter/(box1area + box2area - inter + 1e-6)
